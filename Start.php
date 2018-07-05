581db25fac8f47616928a63950c19cf0b66e9100
davhae <d.haemmerle@protonmail.com>
2018-07-05 21:30:42 +0200

    autoload - load .env, using sentry

---
+++ Start.php
@@ -0,0 +1,43 @@
+<?php
+namespace davhae\example;
+
+use Dotenv\Dotenv;
+use Raven_Autoloader;
+use Raven_Client;
+use Raven_ErrorHandler;
+
+use davhae\example\Utils\Router;
+
+/**
+ * Class Start
+ *
+ * Starts up the thing
+ */
+class Start
+{
+    /**
+     * Start constructor.
+     */
+    public function __construct()
+    {
+        $this->dotenv = new Dotenv(__DIR__);
+        $this->dotenv->load();
+
+        // register sentry handlers
+        Raven_Autoloader::register();
+        $client = new Raven_Client(getenv('SENTRY_DSN'));
+        $error_handler = new Raven_ErrorHandler($client);
+        $error_handler->registerExceptionHandler();
+        $error_handler->registerErrorHandler();
+        $error_handler->registerShutdownFunction();
+    }
+
+    // sentry test
+    public function doSomethingBad()
+    {
+        return 21 / 0;
+    }
+}
+
+$start = new Start;
+//$start->doSomethingBad();
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+namespace davhae\example;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../Start.php';
