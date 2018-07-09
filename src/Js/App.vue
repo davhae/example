d9964203495939b1bd1ad370422c0d29d176a0bc
davhae <d.haemmerle@protonmail.com>
2018-07-09 19:01:03 +0200

    register component in cmp.

--- src/Js/App.vue
+++ src/Js/App.vue
@@ -1,19 +1,18 @@
 <template>
     <div>
         <h1>Hello World!</h1>
-        <h2>TODO:</h2>
-        <ul id="todo">
-            <li>Check how vue components inside other components could work</li>
-            <li>Unit Tests</li>
-            <li>Create a command-ui with vue fe and php BE executing terminal commands</li>
-            <li>Cron provider // REST for creating crons like this site cron-job.org/</li>
-        </ul>
+        <todo></todo>
     </div>
 </template>
 
 <script>
+    // import and define component so we can use it in the template
+    import Todo from './Components/Todo.vue'
     export default {
-        name: "App"
+        name: "App",
+        components: {
+            'todo': Todo
+        }
     }
 </script>
 
